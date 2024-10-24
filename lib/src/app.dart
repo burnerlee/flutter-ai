c778294d552fb0c7f6e4f40b99dd0348edac6cbc
Dashwave <tech@dashwave.io>
2024-10-24 17:55:36 +0000

    Redesign home screen to resemble manga gallery layout.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -62,6 +62,7 @@ class MyApp extends StatelessWidget {
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
+          initialRoute: SampleItemListView.routeName,
           onGenerateRoute: (RouteSettings routeSettings) {
             return MaterialPageRoute<void>(
               settings: routeSettings,
--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'manga_data_source.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
-  const SampleItemDetailsView({super.key});
+  const SampleItemDetailsView({super.key, required this.manga});
 
   static const routeName = '/sample_item';
+
+  final Manga manga;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Item Details'),
+        title: Text(manga.title),
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(manga.thumbnailUrl),
+            const SizedBox(height: 16),
+            Text(
+              manga.title,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Manga ID: ${manga.id}',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            // Additional manga details can be added here
+          ],
+        ),
       ),
     );
   }
